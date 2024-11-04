2d9c103e781217bc96c501fb61c1cfe43682e5aa
MoraFernandoM <morachepo.manuel@gmail.com>
2024-11-04 19:25:07 -0300

    Primer commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Vamos a crear un archivo de prueba para subir al repositorio. En teoría esto es un lector de archivos pandas.
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+archivos = os.listdir()
+
+print(archivos)
